2d87cbe681212fbd674bb04de1c135b38c96b38e
singh <singh.omar97@gmail.com>
2018-08-19 15:36:29 +0200

    media

---
+++ sqls/2 media.sql
@@ -0,0 +1,8 @@
+CREATE TABLE MEDIA (
+    id INTEGER PRIMARY KEY,
+    source VARCHAR(30),
+    typology_type VARCHAR(6),
+    
+    FOREIGN KEY(typology_type) REFERENCES typology(type)
+      ON DELETE SET NULL ON UPDATE CASCADE
+);
